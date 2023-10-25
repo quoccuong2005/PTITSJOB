4853ec8bbe87b0bff36e18a57b8484568bb71a99
chinhcd <7a3dqh@gmail.com>
2023-10-25 17:18:00 +0700

    fix

--- components/Skeleton/BannerhomeSkeleton.tsx
+++ components/Skeleton/BannerhomeSkeleton.tsx
@@ -3,7 +3,7 @@ import Skeleton from "react-loading-skeleton";
 const BannerhomeSkeleton = () => {
   return (
     <>
-      <Skeleton className={"w-full h-[250px] lg:h-auto lg:h-[620px]"} />
+      <Skeleton className={"w-full h-[250px]  lg:h-[620px]"} />
     </>
   );
 };
